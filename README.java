d7aa2ad87125dceb8e3edd19e6b81fa678c3366c
anthony3 <antonyc86@gmail.com>
2013-11-29 15:44:21 +0000

    Update README.java

--- README.java
+++ README.java
@@ -5,5 +5,9 @@ public class Java_Welcome
   public static void main(String[]  args )
   {
     System.out.println("This is a Welcome message !!!");
-    System.out.println("henry modified here !!!");
+    //"henry modified here !!!
+    //"luke was here"
+    //"tomer was here"
+    //"toni is here"
+    //"james is not here , sorry"
 }
